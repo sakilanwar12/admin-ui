daf28591c8e606002bee723787453a7501ad8722
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2025-12-16 13:52:02 +0600

    feat: add LoginForm component and update tsconfig to use react-jsx transform.

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -6,7 +6,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 type Inputs = {
   email: string;
